903d8efa1eb24cfb98a54d1b655344cb41ca1483
Alon Zakai <alonzakai@gmail.com>
2013-04-14 17:23:22 -0700

    use lto

--- emscripten.sh
+++ emscripten.sh
@@ -1,4 +1,3 @@
 echo "bitcode ==> javascript"
-~/Dev/emscripten/emcc -O2 test.c .libs/libgmp.a -o complete.js -s ASM_JS=1
-# --llvm-lto 1
+~/Dev/emscripten/emcc -O2 test.c .libs/libgmp.a -o complete.js -s ASM_JS=1 -g --llvm-lto 1
 
